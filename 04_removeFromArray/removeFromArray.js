7487dad635c41ed55470597a96186f6004371a1a
taomaru <jackson_maria1@columbusstate.edu>
2022-06-26 17:32:20 -0400

    Define ex04 function to pass all tests, account for optional params

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(anArray) {
+    let newArray = [...anArray];
+    let args = [...arguments];
+    args = args.slice(1);
+    for(let i = 0; i < args.length; i++) {
+        let elemIndex = anArray.indexOf(args[i]);
+        if(elemIndex === -1){
+            continue;
+        }
+        newArray = anArray.slice(0, elemIndex);
+        newArray = newArray.concat(anArray.slice(elemIndex + 1));
+        anArray = [...newArray];
+    }
+    return newArray;
 };
 
 // Do not edit below this line
