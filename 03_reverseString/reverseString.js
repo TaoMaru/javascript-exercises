6a86d48673b88c1835c1e78dac9d3cb59cb656a4
taomaru <jackson_maria1@columbusstate.edu>
2022-06-25 21:46:23 -0400

    Define reverseString function to pass all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let strArray = string.split("");
+    let newStr = "";
+    let numChars = strArray.length;
+    for( let i = 0; i < numChars; i++) {
+        newStr += strArray.pop();
+    }
+    return newStr;
 };
 
 // Do not edit below this line
