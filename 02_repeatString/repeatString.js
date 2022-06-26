1f76d208e724fb55427d752476552942ef88c67e
taomaru <jackson_maria1@columbusstate.edu>
2022-06-25 21:29:41 -0400

    Modify ex's 01 & 02 to pass all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, numTimes) {
+    let newString = "";
+    if(numTimes < 0) {
+        newString = "ERROR";
+    }
+    for(let i = 0; i < numTimes; i++) {
+        newString += string;
+    }
+    return newString;
 
 };
 
