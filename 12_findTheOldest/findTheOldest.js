85f7b23838fde5519ca33613d3edb7620a3cdf5d
taomaru <jackson_maria1@columbusstate.edu>
2022-06-27 18:51:23 -0400

    Define findTheOldest to find oldest person, pass all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let max = 1;
+    let oldest;
+    let age;
+    for(const person of people) {
+        if( person.hasOwnProperty("yearOfDeath") ) {
+            age = person.yearOfDeath - person.yearOfBirth;
+        }
+        else {
+            age = 2022 - person.yearOfBirth;
+        }
+        if( age > max) {
+            max = age;
+            oldest = person;
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
