6d3bbc999a1c37e696255ba577a6a90869674034
taomaru <jackson_maria1@columbusstate.edu>
2022-06-27 17:05:11 -0400

    Define palindromes to pass all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (aString) {
+  let inputStr = aString.toLowerCase().split("");
+  
+  let newStr = [];
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+  for(let i = 0; i < inputStr.length; i++) {
+    if( alphabet.includes(inputStr[i] ) ) {
+        newStr.push(inputStr[i]);
+    }
+  }
+  
+  let comparisonStr = [...newStr];
+  comparisonStr.reverse();
+  
+  let joinedNewStr = newStr.join("");
+  let joinedCompStr = comparisonStr.join("");
+  
+  return joinedNewStr === joinedCompStr;
 };
 
 // Do not edit below this line
