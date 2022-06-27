fe831d76bc7f38e73d8dc6efc68d3d33e42689c4
taomaru <jackson_maria1@columbusstate.edu>
2022-06-27 19:15:23 -0400

    Define caesar func to shift correctly, pass all tests

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,37 @@
-const caesar = function() {
-
+const caesar = function(aString, shift) {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    const capitalAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let newStr = "";
+    let oldIndex;
+    let newIndex;
+    for(let i = 0; i < aString.length; i++) {
+        if(alphabet.includes(aString[i])) {
+            oldIndex = alphabet.indexOf(aString[i]);
+            newIndex = oldIndex + shift;
+            if( newIndex >= 26 ) {
+                newIndex = newIndex % 26;
+            }
+            else if( newIndex < 0 ) {
+                newIndex = 26 + newIndex;
+            }
+        newStr += alphabet[newIndex];
+        }
+        else if(capitalAlpha.includes(aString[i])) {
+            oldIndex = capitalAlpha.indexOf(aString[i]);
+            newIndex = oldIndex + shift;
+            if( newIndex >= 26 ) {
+                newIndex = newIndex % 26;
+            }
+            else if (newIndex < 0) {
+                newIndex = 26 + newIndex;
+            }
+        newStr += capitalAlpha[newIndex];
+        }
+        else {
+            newStr += aString[i];
+        }
+    }
+    return newStr;
 };
 
 // Do not edit below this line
