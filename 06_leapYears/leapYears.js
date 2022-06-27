43d709ea416f9e2ad7114ed7b62d1248438af18d
taomaru <jackson_maria1@columbusstate.edu>
2022-06-26 22:06:12 -0400

    Define ex06 function to determine leapyears, pass all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(aYear) {
+    if(typeof aYear == "number") {
+        if(aYear % 4 == 0) {
+            if(aYear % 100 == 0) {
+                return (aYear % 400 == 0);
+            }
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
